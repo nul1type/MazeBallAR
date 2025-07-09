9fe7454beb0eb442f2d1369dd3a8b3ab96cc42a7
Anatoly Aleksandrov <aleksandrov_a_a@icloud.com>
2025-07-09 13:01:47 +0700

    Initial Commit

---
+++ MazeBallARTests/MazeBallARTests.swift
@@ -0,0 +1,17 @@
+//
+//  MazeBallARTests.swift
+//  MazeBallARTests
+//
+//  Created by Анатолий Александрович on 09.07.2025.
+//
+
+import Testing
+@testable import MazeBallAR
+
+struct MazeBallARTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
